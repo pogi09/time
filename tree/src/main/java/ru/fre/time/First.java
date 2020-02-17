09f493910c24cab3e0e9bdc666505cfbe4698d2b
POgi09 <polischykpogi@gmail.com>
2020-02-17 14:54:17 +0300

    Добавил несколько функций

--- tree/src/main/java/ru/fre/time/First.java
+++ tree/src/main/java/ru/fre/time/First.java
@@ -2,6 +2,26 @@ package ru.fre.time;
 
 public class First {
 	public static void main (String[] args){
-		System.out.println("Hi man!");
-	}	
+		nHi("man");
+		nHi("friends");
+
+		double  k = 11;
+		System.out.println("Пл стор кв" + k + "=" +area(k));
+
+		double  s = 6;
+		double r = 4;
+		System.out.println("Пл стор прямоуг " + s +"и"+ r + " = " +area(s,r));
+	}
+
+	public static void nHi(String somebody) {
+		System.out.println("Hi, " + somebody + "!");
+	}
+	public static double area(double k){
+		 return k*k;
+	}
+
+	public static double area (double s, double r){
+		return s*r;
+	}
+
 }
